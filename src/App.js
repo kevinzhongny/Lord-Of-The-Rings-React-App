7a59e6d410f3f004133ba8770fc6b130c9fab861
Kevin Zhong <kevinzhongny@gmail.com>
2021-01-19 12:58:33 -0500

    'test_host'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const CHARACTERS_URL = 'https://the-one-api.dev/v2/character?sort=name:asc';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/Characters">Characters</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/characters">
+              <Characters />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+  
+}
+
+class Characters extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = [];
+  }
+
+  async getCharacters() {
+    try{
+      const res = await axios.get(CHARACTERS_URL, {headers: {Authorization: 'Bearer 9uTC8uqZwMP1lQXd-bhV'}});
+      this.setState({ characters: res.data.docs })
+    } catch(e) {
+      console.error(e.message);
+    }
+
+  }
+
+  componentDidMount() {
+    this.getCharacters();
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {
+          this.state.characters && this.state.characters.map(characters => (
+            <li key={ characters.id }>
+              { characters.name }
+              <ul>
+                <li>Race: { characters.race }</li>
+                <li>Gender: { characters.gender }</li>
+                <li>Birth: { characters.birth }</li>
+                <li>Death: { characters.death }</li>
+                <li>Realm: { characters.realm }</li>
+                <li>WikiURL: <a href={ characters.wikiUrl }>{ characters.wikiUrl }</a></li>
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+function Home() {
+  return <h2>Home</h2>;
 }
 
 export default App;
